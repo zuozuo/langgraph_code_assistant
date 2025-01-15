6585a7bd16aebbcb5cbbd6221f0f2116e90e68a5
左永辉 <zuoyonghui@bytedance.com>
2025-01-15 19:37:33 +0800

    init

---
+++ demo.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup as Soup
+from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
+
+# LCEL docs
+url = "https://python.langchain.com/docs/concepts/lcel/"
+loader = RecursiveUrlLoader(
+    url=url, max_depth=20, extractor=lambda x: Soup(x, "html.parser").text
+)
+docs = loader.load()
+
+# Sort the list based on the URLs and get the text
+d_sorted = sorted(docs, key=lambda x: x.metadata["source"])
+d_reversed = list(reversed(d_sorted))
+concatenated_content = "\n\n\n --- \n\n\n".join(
+    [doc.page_content for doc in d_reversed]
+)
+
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+### OpenAI
+
+# Grader prompt
+code_gen_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are a coding assistant with expertise in LCEL, LangChain expression language. \n 
+    Here is a full set of LCEL documentation:  \n ------- \n  {context} \n ------- \n Answer the user 
+    question based on the above provided documentation. Ensure any code you provide can be executed \n 
+    with all required imports and variables defined. Structure your answer with a description of the code solution. \n
+    Then list the imports. And finally list the functioning code block. Here is the user question:""",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
+
+
+# Data model
+class code(BaseModel):
+    """Schema for code solutions to questions about LCEL."""
+
+    prefix: str = Field(description="Description of the problem and approach")
+    imports: str = Field(description="Code block import statements")
+    code: str = Field(description="Code block not including import statements")
+
+
+# llm = ChatOpenAI(temperature=0, model="anthropic/claude-3.5-sonnet")
+llm = ChatOpenAI(temperature=0, model="openai/gpt-4o")
+code_gen_chain_oai = code_gen_prompt | llm.with_structured_output(code)
+question = "How do I build a RAG chain in LCEL?"
+solution = code_gen_chain_oai.invoke(
+    {"context": concatenated_content, "messages": [("user", question)]}
+)
+solution
